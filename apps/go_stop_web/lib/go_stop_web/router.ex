4587090744ffee3a0a68cd06ec975ec8c4d0bed2
Cam <cjirmas@gmail.com>
2018-03-06 22:27:32 -0800

    Add absinthe

--- apps/go_stop_web/lib/go_stop_web/router.ex
+++ apps/go_stop_web/lib/go_stop_web/router.ex
@@ -5,7 +5,13 @@ defmodule GoStopWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/api", GoStopWeb do
+  scope "/api" do
     pipe_through :api
+
+    forward "/graphiql", Absinthe.Plug.GraphiQL,
+      schema: GoStopWeb.Schema
+
+    forward "/", Absinthe.Plug,
+      schema: GoStopWeb.Schema
   end
 end
