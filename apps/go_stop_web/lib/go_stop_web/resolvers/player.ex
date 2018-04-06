2aff83bfb3314f1f5ebb1eff66d45c7bdc6b7cd1
Cam <cjirmas@gmail.com>
2018-04-05 20:13:21 -0700

    Remove createPlayer mutation
    We will no longer need this, since it will be covered in the
    createGame mutation.

--- apps/go_stop_web/lib/go_stop_web/resolvers/player.ex
+++ apps/go_stop_web/lib/go_stop_web/resolvers/player.ex
@@ -1,11 +1,4 @@
 defmodule GoStopWeb.Resolvers.Player do
-  def create_player(_parent, data, _resolution) do
-    case GoStop.Player.create(data) do
-      {:ok, _} = player -> player
-      {:error, changeset} -> {:error, "Failed: #{parse_errors(changeset)}"}
-    end
-  end
-
   def get_player(_parent, %{id: id}, _resolution) do
     {:ok, GoStop.Player.get(id, preload: [:user, :game])}
   end
