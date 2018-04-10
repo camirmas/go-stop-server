01d9c603571634c1b4f528bccb8f9c8c8b49e4b0
Cam <cjirmas@gmail.com>
2018-04-09 23:59:26 -0700

    Add pass mutation

--- apps/go_stop_web/lib/go_stop_web/resolvers/player.ex
+++ apps/go_stop_web/lib/go_stop_web/resolvers/player.ex
@@ -1,6 +1,80 @@
 defmodule GoStopWeb.Resolvers.Player do
+  alias Ecto.Multi
+
+  alias GoStop.{Repo, Player, Game}
+
   def get_player(_parent, %{id: id}, _resolution) do
-    {:ok, GoStop.Player.get(id, preload: [:user, :game])}
+    {:ok, Player.get(id, preload: [:user, :game])}
+  end
+
+  def player_pass(_parent, %{game_id: game_id},
+                  %{context: %{current_user: current_user}}) do
+    game = get_game(game_id)
+
+    changeset = Player.changeset(%Player{}, %{})
+
+    if game do
+      player = get_player(game, current_user)
+
+      if player do
+        player = player |> Repo.preload(:game)
+
+        if player_turn?(player) do
+          update_player(game, player)
+        else
+          {:error, "Failed: player must wait a turn"}
+        end
+      else
+        {:error, "Failed: player does not exist"}
+      end
+    else
+      {:error, "Failed: game does not exist"}
+    end
+  end
+
+  def player_pass(_, _, _) do
+    {:error, "Failed: user not authenticated"}
+  end
+
+  defp update_player(game, player) do
+    multi =
+      Multi.new
+      |> Multi.run(:player, fn _ ->
+        Player.update(player, %{has_passed: true})
+      end)
+      |> Multi.run(:game, fn %{player: player} ->
+        players = Player.list_for_game(game)
+
+        if Enum.all?(players, &(&1.has_passed)) do
+          # TODO: run score calculation
+          Game.update(game, %{status: "complete"})
+        else
+          new_player = get_new_player(game, player)
+          Game.update(game, %{player_turn_id: new_player.id})
+        end
+      end)
+
+    case Repo.transaction(multi) do
+      {:ok, %{player: player}} -> {:ok, player}
+      {:error, _name, changeset, _} ->
+        {:error, "Failed: #{parse_errors(changeset)}"}
+    end
+  end
+
+  defp get_game(game_id) do
+    Game.get(game_id, preload: [:players])
+  end
+
+  defp get_player(game, current_user) do
+    Enum.find(game.players, fn p -> p.user_id == current_user.id end)
+  end
+
+  defp player_turn?(player) do
+    player && player.id == player.game.player_turn_id
+  end
+
+  defp get_new_player(game, player) do
+    Enum.find(game.players, fn p -> p.id != player.id end)
   end
 
   defp parse_errors(changeset) do
--- apps/go_stop_web/test/go_stop_web/schema/player_test.exs
+++ apps/go_stop_web/test/go_stop_web/schema/player_test.exs
@@ -1,6 +1,10 @@
 defmodule GoStopWeb.Schema.PlayerTest do
   use GoStopWeb.ConnCase, async: true
 
+  import GoStopWeb.Guardian
+
+  alias GoStop.{Repo, Player, Game}
+
   describe "player" do
     setup do
       [player: insert(:player)]
@@ -37,6 +41,62 @@ defmodule GoStopWeb.Schema.PlayerTest do
         |> json_response(200)
 
       assert res == %{"data" => %{"player" => nil}}
+    end
+  end
+
+  describe "pass" do
+    setup do
+      game = insert(:game)
+      player = insert(:player, %{game: game, color: "black"})
+      opponent = insert(:player, %{game: game, color: "white"})
+      game = game |> Repo.preload(:players)
+      {:ok, _} = Game.update(game, %{player_turn_id: player.id})
+
+      {:ok, token, _} = encode_and_sign(player.user, %{}, token_type: :access)
+
+      [token: token, player: player, opponent: opponent]
+    end
+
+    test "allows a Player to pass on a turn",
+      %{conn: conn, player: player, opponent: opponent, token: token} do
+        query = """
+        mutation Pass {
+          pass(gameId: #{player.game_id}) {
+            id
+          }
+        }
+        """
+        res =
+          conn
+          |> put_req_header("authorization", "Bearer " <> token)
+          |> post("/api", %{query: query})
+          |> json_response(200)
+
+        assert res == %{"data" => %{"pass" => %{"id" => "#{player.id}"}}}
+        player = Player.get(player.id, preload: [:game])
+        assert player.has_passed
+        assert player.game.player_turn_id == opponent.id
+    end
+
+    test "ends the Game when both Players pass",
+      %{conn: conn, player: player, opponent: opponent, token: token} do
+        {:ok, _} = Player.update(opponent, %{has_passed: true})
+        query = """
+        mutation Pass {
+          pass(gameId: #{player.game_id}) {
+            id
+          }
+        }
+        """
+        res =
+          conn
+          |> put_req_header("authorization", "Bearer " <> token)
+          |> post("/api", %{query: query})
+          |> json_response(200)
+
+        assert res == %{"data" => %{"pass" => %{"id" => "#{player.id}"}}}
+        game = Game.get(player.game_id)
+        assert game.status == "complete"
     end
   end
 end
