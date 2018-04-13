448ed472b34a822a1d41acaa943700d8849ed507
Cam <cjirmas@gmail.com>
2018-04-13 01:17:17 -0700

    Refactor Player Resolver

--- apps/go_stop_web/lib/go_stop_web/resolvers/player.ex
+++ apps/go_stop_web/lib/go_stop_web/resolvers/player.ex
@@ -12,41 +12,53 @@ defmodule GoStopWeb.Resolvers.Player do
            %{context: %{current_user: current_user}}) do
     player = Player.get_by(%{game_id: game_id, user_id: current_user.id})
 
-    if player do
-      player = player |> Repo.preload(:game)
-
-      if Player.is_turn?(player) do
-        update_player(player)
-      else
-        wrong_turn_error()
-      end
-    else
-      game_not_found_error()
-    end
+    pass_turn(player)
   end
 
   def pass(_, _, _) do
     authentication_error()
   end
 
-  defp update_player(%{game: game} = player) do
-    multi =
-      Multi.new
-      |> Multi.run(:player, fn _ ->
-        Player.update(player, %{has_passed: true})
-      end)
-      |> Multi.run(:game, fn %{player: player} ->
-        players = Player.list_for_game(game)
+  defp pass_turn(nil) do
+    game_not_found_error()
+  end
+  defp pass_turn(player) do
+    player
+    |> Repo.preload([:game])
+    |> validate_turn()
+    |> build_turn()
+    |> execute_turn()
+  end
 
-        if Enum.all?(players, &(&1.has_passed)) do
-          # TODO: run score calculation
-          Game.update(game, %{status: "complete"})
-        else
-          new_player = Player.get_opponent(player)
-          Game.update(game, %{player_turn_id: new_player.id})
-        end
-      end)
+  defp validate_turn(player) do
+    if Player.is_turn?(player) do
+      player
+    else
+      wrong_turn_error()
+    end
+  end
 
+  defp build_turn(%{game: game} = player) do
+    Multi.new
+    |> Multi.run(:player, fn _ ->
+      Player.update(player, %{has_passed: true})
+    end)
+    |> Multi.run(:game, fn %{player: player} ->
+      players = Player.list_for_game(game)
+
+      if Enum.all?(players, &(&1.has_passed)) do
+        # TODO: run score calculation
+        Game.update(game, %{status: "complete"})
+      else
+        new_player = Player.get_opponent(player)
+        Game.update(game, %{player_turn_id: new_player.id})
+      end
+    end)
+  end
+  defp build_turn({:error, _} = err), do: err
+
+  defp execute_turn({:error, _} = err), do: err
+  defp execute_turn(multi) do
     case Repo.transaction(multi) do
       {:ok, %{player: player}} -> {:ok, player}
       {:error, _name, changeset, _} ->
