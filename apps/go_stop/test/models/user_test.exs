c39b1b07220622e2e02e6f18119a3112b6424c3a
Cam <cjirmas@gmail.com>
2018-04-15 23:39:03 -0700

    Downcase email before saving to DB

--- apps/go_stop/test/models/user_test.exs
+++ apps/go_stop/test/models/user_test.exs
@@ -1,6 +1,7 @@
 defmodule UserTest do
   use GoStop.DataCase, async: true
 
+  import Ecto.Changeset, only: [get_change: 2]
   alias GoStop.User
 
   @params %{
@@ -10,13 +11,20 @@ defmodule UserTest do
   }
 
   describe "changesets" do
-    test "changeset is valid with username, email, password" do
+    test "is valid with username, email, password" do
       changeset = User.registration_changeset(%User{}, @params)
 
       assert is_valid(changeset)
     end
 
-    test "changeset is invalid with improper email format" do
+    test "downcases emails" do
+      params = %{@params | email: String.upcase(@params.email)}
+      changeset = User.registration_changeset(%User{}, params)
+
+      assert get_change(changeset, :email) == @params.email
+    end
+
+    test "is invalid with improper email format" do
       invalid_emails = ~w(user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com foo@bar..com)
       Enum.each(invalid_emails, fn email ->
@@ -27,7 +35,7 @@ defmodule UserTest do
       end)
     end
 
-    test "changeset is invalid with improper password length" do
+    test "is invalid with improper password length" do
       params = %{@params | password: "short"}
       changeset = User.registration_changeset(%User{}, params)
 
