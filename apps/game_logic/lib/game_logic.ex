c9227a3dd68b8ad17520612d2e78b8bc83e80a61
Cam <cirmas@protonmail.com>
2019-01-02 12:36:22 -0800

    Implement basic liberties

--- apps/game_logic/lib/game_logic.ex
+++ apps/game_logic/lib/game_logic.ex
@@ -1,18 +1,132 @@
 defmodule GameLogic do
-  @moduledoc """
-  Documentation for GameLogic.
-  """
+  defmodule Board do
+    defstruct size: 13, stones: []
+  end
 
-  @doc """
-  Hello world.
+  defmodule Stone do
+    defstruct x: 0, y: 0, color: nil
+  end
 
-  ## Examples
+  def run(%Board{stones: stones} = board) do
+    board
+    |> get_chains
+    |> Enum.map(fn chain -> find_liberties(board, chain) end)
+    |> Enum.filter(fn {chain, liberties} -> liberties == 0 end)
+    |> Enum.map(fn {chain, liberties} -> chain end)
+  end
 
-      iex> GameLogic.hello
-      :world
+  def get_chains(%Board{stones: stones} = board) do
+    stones
+    |> Enum.map(fn stone ->
+      board
+      |> get_chain(stone)
+      |> Enum.sort_by(fn %{x: x, y: y} -> {x, y} end)
+    end)
+    |> Enum.dedup()
+  end
 
-  """
-  def hello do
-    :world
+  def find_liberties(board, chain) do
+    liberties = chain
+      |> Enum.reduce([], fn(stone, total_acc) ->
+        individual_liberties = board
+          |> get_nearby_liberties(stone)
+          |> Enum.reduce([], fn (%{color: color} = stone, acc_i) ->
+            if is_nil(color) do
+              [stone | acc_i]
+            else
+              acc_i
+            end
+          end)
+
+        total_acc ++ individual_liberties
+      end)
+      |> List.flatten()
+      |> Enum.dedup()
+
+    {chain, length(liberties)}
+  end
+
+  defp get_chain(board, %Stone{} = stone) do
+    get_chain_helper(board, [stone], [], stone.color)
+  end
+
+  defp get_chain_helper(_, [], chain, color), do: chain
+
+  defp get_chain_helper(board, acc, chain, color) do
+    [s | acc] = acc
+
+    if s not in chain && s.color == color do
+      chain = [s | chain]
+
+      acc =
+        board
+        |> get_nearby_stones(s)
+        |> Enum.concat(acc)
+
+      get_chain_helper(board, acc, chain, color)
+    else
+      get_chain_helper(board, acc, chain, color)
+    end
+  end
+
+  defp get_nearby_stones(%Board{stones: stones, size: size}, s) do
+    up = %Stone{x: s.x, y: s.y + 1}
+    down = %Stone{x: s.x, y: s.y - 1}
+    left = %Stone{x: s.x - 1, y: s.y}
+    right = %Stone{x: s.x + 1, y: s.y}
+    top_left = %Stone{x: s.x - 1, y: s.y + 1}
+    top_right = %Stone{x: s.x + 1, y: s.y + 1}
+    bottom_left = %Stone{x: s.x - 1, y: s.y - 1}
+    bottom_right = %Stone{x: s.x + 1, y: s.y - 1}
+
+    [up, down, left, right, top_left, top_right, bottom_left, bottom_right]
+    |> Enum.filter(fn stone -> is_inbounds(stone, size) end)
+    |> Enum.map(fn nearby ->
+      stone = Enum.find(stones, fn stone ->
+        {stone.x, stone.y} == {nearby.x, nearby.y}
+      end)
+
+      if stone do
+        %{nearby | color: stone.color}
+      else
+        nearby
+      end
+    end)
+  end
+
+  def get_nearby_liberties(%Board{stones: stones, size: size}, s) do
+    up = %Stone{x: s.x, y: s.y + 1}
+    down = %Stone{x: s.x, y: s.y - 1}
+    left = %Stone{x: s.x - 1, y: s.y}
+    right = %Stone{x: s.x + 1, y: s.y}
+
+    [up, down, left, right]
+      |> Enum.filter(fn stone -> is_inbounds(stone, size) end)
+      |> Enum.map(fn nearby ->
+        stone = Enum.find(stones, fn stone ->
+          {stone.x, stone.y} == {nearby.x, nearby.y}
+        end)
+
+        if stone do
+          %{nearby | color: stone.color}
+        else
+          nearby
+        end
+      end)
+  end
+
+  defp is_inbounds(%Stone{x: x, y: y}, size) do
+    cond do
+      x < 0 ->
+        false
+      x >= size ->
+        false
+      y < 0 ->
+        false
+      y >= size ->
+        false
+      true ->
+        true
+    end
   end
 end
--- apps/game_logic/test/game_logic_test.exs
+++ apps/game_logic/test/game_logic_test.exs
@@ -1,8 +1,68 @@
 defmodule GameLogicTest do
   use ExUnit.Case
-  doctest GameLogic
 
-  test "greets the world" do
-    assert GameLogic.hello() == :world
+  alias GameLogic.{Board, Stone}
+
+  test "gets linear chains" do
+    s1 = %Stone{x: 0, y: 0, color: :black}
+    s2 = %Stone{x: 1, y: 1, color: :white}
+    s3 = %Stone{x: 2, y: 2, color: :white}
+    s4 = %Stone{x: 3, y: 3, color: :white}
+
+    b = %Board{
+      size: 13,
+      stones: [s1, s2, s3, s4]
+    }
+
+    [b | _] = chains = GameLogic.get_chains(b)
+
+    assert length(chains) == 2
+    assert b == [s1]
+
+    w = List.last(chains)
+    assert length(w) == 3
+    assert w == [s2, s3, s4]
+  end
+
+  test "gets circular chains" do
+    s1 = %Stone{x: 1, y: 2, color: :white}
+    s2 = %Stone{x: 2, y: 1, color: :white}
+    s3 = %Stone{x: 0, y: 1, color: :white}
+    s4 = %Stone{x: 1, y: 0, color: :white}
+
+    b = %Board{
+      size: 13,
+      stones: [s1, s2, s3, s4]
+    }
+
+    [c | _] = chains = GameLogic.get_chains(b)
+
+    assert length(chains) == 1
+    assert c == [s3, s4, s1, s2]
+  end
+
+  test "#run with basic liberties" do
+    s1 = %Stone{x: 1, y: 2, color: :white}
+    s2 = %Stone{x: 2, y: 1, color: :white}
+    s3 = %Stone{x: 0, y: 1, color: :white}
+    s4 = %Stone{x: 1, y: 0, color: :white}
+    s5 = %Stone{x: 1, y: 1, color: :black} # No liberties
+
+    s6 = %Stone{x: 12, y: 0, color: :white} # No liberties
+    s7 = %Stone{x: 12, y: 1, color: :black}
+    s8 = %Stone{x: 11, y: 0, color: :black}
+
+    b = %Board{
+      size: 13,
+      stones: [s1, s2, s3, s4, s5, s6, s7, s8]
+    }
+
+    [to_remove | rest] = chains = GameLogic.run(b)
+
+    assert length(chains) == 2
+    assert to_remove == [s5]
+
+    [to_remove2 | _] = rest
+    assert to_remove2 == [s6]
   end
 end
