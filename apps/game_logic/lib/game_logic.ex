ecf3e919b9ee1d2e8b45e628bc10b4706cc4e698
Cam <cirmas@protonmail.com>
2019-01-02 18:50:04 -0800

    Implement stone capture rules for strings

--- apps/game_logic/lib/game_logic.ex
+++ apps/game_logic/lib/game_logic.ex
@@ -7,30 +7,34 @@ defmodule GameLogic do
     defstruct x: 0, y: 0, color: nil
   end
 
-  def run(%Board{stones: stones} = board) do
+  def run(%Board{} = board, %Stone{} = added_stone) do
     board
-    |> get_chains
-    |> Enum.map(fn chain -> find_liberties(board, chain) end)
-    |> Enum.filter(fn {chain, liberties} -> liberties == 0 end)
-    |> Enum.map(fn {chain, liberties} -> chain end)
+    |> get_strings
+    # TODO: modify to prevent suicidal moves (see issue #25)
+    |> Enum.filter(fn string -> added_stone not in string end)
+    |> Enum.map(fn string -> find_liberties(board, string) end)
+    |> Enum.filter(fn {_string, liberties} -> liberties == 0 end)
+    |> Enum.map(fn {string, _liberties} -> string end)
   end
 
-  def get_chains(%Board{stones: stones} = board) do
+  def get_strings(%Board{stones: stones} = board) do
     stones
     |> Enum.map(fn stone ->
       board
-      |> get_chain(stone)
+      |> get_string(stone)
       |> Enum.sort_by(fn %{x: x, y: y} -> {x, y} end)
     end)
     |> Enum.dedup()
   end
 
-  def find_liberties(board, chain) do
-    liberties = chain
-      |> Enum.reduce([], fn(stone, total_acc) ->
-        individual_liberties = board
-          |> get_nearby_liberties(stone)
-          |> Enum.reduce([], fn (%{color: color} = stone, acc_i) ->
+  def find_liberties(board, string) do
+    liberties =
+      string
+      |> Enum.reduce([], fn stone, total_acc ->
+        individual_liberties =
+          board
+          |> get_nearby(stone)
+          |> Enum.reduce([], fn %{color: color} = stone, acc_i ->
             if is_nil(color) do
               [stone | acc_i]
             else
@@ -43,48 +47,45 @@ defmodule GameLogic do
       |> List.flatten()
       |> Enum.dedup()
 
-    {chain, length(liberties)}
+    {string, length(liberties)}
   end
 
-  defp get_chain(board, %Stone{} = stone) do
-    get_chain_helper(board, [stone], [], stone.color)
+  defp get_string(board, %Stone{} = stone) do
+    get_string_helper(board, [stone], [], stone.color)
   end
 
-  defp get_chain_helper(_, [], chain, color), do: chain
+  defp get_string_helper(_board, [], string, _color), do: string
 
-  defp get_chain_helper(board, acc, chain, color) do
+  defp get_string_helper(board, acc, string, color) do
     [s | acc] = acc
 
-    if s not in chain && s.color == color do
-      chain = [s | chain]
+    if s not in string && s.color == color do
+      string = [s | string]
 
       acc =
         board
-        |> get_nearby_stones(s)
+        |> get_nearby(s)
         |> Enum.concat(acc)
 
-      get_chain_helper(board, acc, chain, color)
+      get_string_helper(board, acc, string, color)
     else
-      get_chain_helper(board, acc, chain, color)
+      get_string_helper(board, acc, string, color)
     end
   end
 
-  defp get_nearby_stones(%Board{stones: stones, size: size}, s) do
+  defp get_nearby(%Board{stones: stones, size: size}, s) do
     up = %Stone{x: s.x, y: s.y + 1}
     down = %Stone{x: s.x, y: s.y - 1}
     left = %Stone{x: s.x - 1, y: s.y}
     right = %Stone{x: s.x + 1, y: s.y}
-    top_left = %Stone{x: s.x - 1, y: s.y + 1}
-    top_right = %Stone{x: s.x + 1, y: s.y + 1}
-    bottom_left = %Stone{x: s.x - 1, y: s.y - 1}
-    bottom_right = %Stone{x: s.x + 1, y: s.y - 1}
 
-    [up, down, left, right, top_left, top_right, bottom_left, bottom_right]
+    [up, down, left, right]
     |> Enum.filter(fn stone -> is_inbounds(stone, size) end)
     |> Enum.map(fn nearby ->
-      stone = Enum.find(stones, fn stone ->
-        {stone.x, stone.y} == {nearby.x, nearby.y}
-      end)
+      stone =
+        Enum.find(stones, fn stone ->
+          {stone.x, stone.y} == {nearby.x, nearby.y}
+        end)
 
       if stone do
         %{nearby | color: stone.color}
@@ -94,37 +95,20 @@ defmodule GameLogic do
     end)
   end
 
-  def get_nearby_liberties(%Board{stones: stones, size: size}, s) do
-    up = %Stone{x: s.x, y: s.y + 1}
-    down = %Stone{x: s.x, y: s.y - 1}
-    left = %Stone{x: s.x - 1, y: s.y}
-    right = %Stone{x: s.x + 1, y: s.y}
-
-    [up, down, left, right]
-      |> Enum.filter(fn stone -> is_inbounds(stone, size) end)
-      |> Enum.map(fn nearby ->
-        stone = Enum.find(stones, fn stone ->
-          {stone.x, stone.y} == {nearby.x, nearby.y}
-        end)
-
-        if stone do
-          %{nearby | color: stone.color}
-        else
-          nearby
-        end
-      end)
-  end
-
   defp is_inbounds(%Stone{x: x, y: y}, size) do
     cond do
       x < 0 ->
         false
+
       x >= size ->
         false
+
       y < 0 ->
         false
+
       y >= size ->
         false
+
       true ->
         true
     end
